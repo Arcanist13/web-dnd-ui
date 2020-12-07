273e7854fc08f6b35f3b6823f042679db43f0531
a1749844 <rutleya@gmail.com>
2020-12-07 14:55:49 +1030

    location change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUri: 'http://' + window.location.hostname + ':5000/spell'
+  backendUri: 'http://' + window.location.hostname + ':5000'
 };
 
 /*
